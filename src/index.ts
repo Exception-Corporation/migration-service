e80382732dc3f5af013017ebe0395d701a411ca1
Edgar Castillo Vega <vayne.edgar@gmail.com>
2023-06-22 14:10:49 -0600

    feat: the restart configuration was added in the app service from the docker-compose containers with walways value

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+import http from "http";
 import app from "./app";
 import config from "./config";
 import { AppDataSource } from "./db";
+import { WebSocket } from "./websocket/socket.io";
 
 async function main() {
   try {
     await AppDataSource.initialize();
     console.log("Database Connected");
 
+    const server = http.createServer(app);
+
+    new WebSocket(server).initialize();
+
     app.listen(config.server.port);
+
     console.log("Server is listening on port", config.server.port);
   } catch (error: any) {
     console.log(error);
---
+++ src/websocket/socket.io.ts
@@ -0,0 +1,26 @@
+import { Server as SocketServer } from "socket.io";
+
+export class WebSocket {
+  private io: SocketServer;
+  constructor(private server: any) {
+    this.io = new SocketServer(this.server, {
+      cors: {
+        origin: "*",
+      },
+      allowEIO3: true,
+      transports: ["polling", "websocket"],
+    });
+  }
+
+  initialize() {
+    this.io.on("connection", (socket) => {
+      console.log(socket.id);
+      socket.on("message", (body) => {
+        socket.broadcast.emit("message", {
+          body,
+          from: socket.id.slice(8),
+        });
+      });
+    });
+  }
+}
